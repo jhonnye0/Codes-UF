16f730f31f8fa89fedc85e6a944206cb93fdf1d1
jhonnye0 <43444151+jhonnye0@users.noreply.github.com>
2018-12-15 22:58:00 -0200

    Update Mergesort.c

--- Mergesort.c
+++ Mergesort.c
@@ -3,8 +3,7 @@
 
 void merge(int *v, int in, int fn, int mid)
 {
-	int n1, n2, i, j, k;
-    
+	int n1, n2, i, j, k;    
 	n1 = (mid - in)+1;
 	n2 = (fn - mid);
 
@@ -16,8 +15,7 @@ void merge(int *v, int in, int fn, int mid)
 	for (i = 0; i < n2; ++i)
 	{
 		R[i] = v[mid + i + 1];
-	}
-    
+	}    
     i=0;
     j=0;
     k=in;
@@ -36,8 +34,7 @@ void merge(int *v, int in, int fn, int mid)
 			j++;
 			k++;
 		}
-	}
-	
+	}	
 	while(i<n1)
 	{
 	    v[k] = L[i];
@@ -61,18 +58,14 @@ void mergesort(int *v, int in , int fn)
 	    mergesort(v, mid+1, fn);
 	    
 	    merge(v, in, fn, mid);
-	}
-	
+	}	
 }
 
 int main()
 {
-	int *v = NULL;
-	int n, i=0;
-
-	v = (int *) malloc(sizeof(int));
-
-	scanf("%d", &n);
+	int *v = NULL; v = (int *) malloc(sizeof(int));
+	int n, i=0; scanf("%d", &n);	
+	
 	while(n != -1)
 	{		
 		v[i] = n;
@@ -81,98 +74,6 @@ int main()
 		scanf("%d", &n);
 	}
 	int size = i;
-
-	mergesort(v, 0, size-1);
-
-	for (i = 0; i < size; ++i)
-	{
-		(i==size-1) ? printf("%d\n", v[i]) : printf("%d ", v[i]);
-	}
-	return 0;
-}#include <stdio.h>
-#include <limits.h>
-
-void merge(int *v, int in, int fn, int mid)
-{
-	int n1, n2, i, j, k;
-    
-	n1 = (mid - in)+1;
-	n2 = (fn - mid);
-
-	int L[n1], R[n2];
-	for (i = 0; i < n1; ++i)
-	{
-		L[i] = v[in + i];
-	}
-	for (i = 0; i < n2; ++i)
-	{
-		R[i] = v[mid + i + 1];
-	}
-    
-    i=0;
-    j=0;
-    k=in;
-
-	while(i < n1 && j < n2)
-	{
-		if(L[i] <= R[j])
-		{
-			v[k] = L[i];
-			i++;
-			k++;
-		}
-		else
-		{
-			v[k] = R[j];
-			j++;
-			k++;
-		}
-	}
-	
-	while(i<n1)
-	{
-	    v[k] = L[i];
-	    i++;
-	    k++;
-	}
-	while(j<n2)
-	{
-	    v[k] = R[j];
-	    j++;
-	    k++;
-	}
-}
-
-void mergesort(int *v, int in , int fn)
-{
-	if(in < fn)
-	{
-	    int mid = in+(fn-in)/2;
-	    mergesort(v, in , mid);
-	    mergesort(v, mid+1, fn);
-	    
-	    merge(v, in, fn, mid);
-	}
-	
-}
-
-int main()
-{
-	int *v = NULL;
-	int n, i=0;
-
-	v = (int *) malloc(sizeof(int));
-
-	scanf("%d", &n);
-	while(n != -1)
-	{		
-		v[i] = n;
-		v = (int *) realloc(v, sizeof(int)*(i+2));
-		i++;
-		scanf("%d", &n);
-	}
-	int size = i;
-
 	mergesort(v, 0, size-1);
 
 	for (i = 0; i < size; ++i)
