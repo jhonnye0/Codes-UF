e044b7af33fc7e675335bd0e8497958bb2f4508f
jhonnye0 <43444151+jhonnye0@users.noreply.github.com>
2018-12-22 14:32:38 -0200

    Create FibonacciDP.cpp

---
+++ FibonacciDP.cpp
@@ -0,0 +1,31 @@
+// fib DP
+#include <bits/stdc++>
+
+#define MAX 1000000
+
+using namespace std;
+
+int n, dp[MAX];
+
+int fib(int x)
+{
+	if(dp[x]!=-1) return dp[x];
+
+	dp[x] = fib(x-1) + fib(x-2);
+
+	return dp[x];
+}
+
+int main()
+{
+	cin >> n; // scaneio
+
+	memset(dp, -1, sizeof(dp)); //faço todos iguais a -1
+
+	dp[0] = 0; // caso base
+	dp[1] = 1;
+
+	cout << fib(n) << endl; // imprimo o valor
+
+	return 0;
+}
